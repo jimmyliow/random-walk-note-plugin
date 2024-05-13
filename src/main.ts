bb06425d18ce93b7e41cefda2d5dfe5524285040
Jimmy Liow <lyj@hainatravel.com>
2024-05-13 15:53:17 +0800

    增加自定义图标，重复三次后忽略

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {  Plugin, TFile, Notice } from 'obsidian';
+import {  Plugin, TFile, Notice, addIcon } from 'obsidian';
 import { getTagFilesMap, randomElement } from './utilities';
 import { RandomWalkNoteSettingTab } from './settingTab';
 import {  RandomWalkNoteSettings } from './types';
@@ -10,8 +10,12 @@ export default class RandomWalkNotePlugin extends Plugin {
     ribbonIconEl: HTMLElement | undefined = undefined;
 
     async onload(): Promise<void> {
+      // 文档：https://docs.obsidian.md/Plugins/User+interface/Icons
+      // 参考自定义图标：
+      // https://github.com/RafaelGB/obsidian-db-folder/blob/master/src/helpers/Constants.ts
+      // https://github.com/zsviczian/obsidian-excalidraw-plugin/blob/master/src/constants/constants.ts#L396
+      addIcon('random-walk-icon', `<g transform="translate(5,5) scale(4,4)" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 16v-2.38C4 11.5 2.97 10.5 3 8c.03-2.72 1.49-6 4.5-6C9.37 2 10 3.8 10 5.5c0 3.11-2 5.66-2 8.68V16a2 2 0 1 1-4 0Z"/><path d="M20 20v-2.38c0-2.12 1.03-3.12 1-5.62-.03-2.72-1.49-6-4.5-6C14.63 6 14 7.8 14 9.5c0 3.11 2 5.66 2 8.68V20a2 2 0 1 0 4 0Z"/><path d="M16 17h4"/><path d="M4 13h4"/></g>`);
         await this.loadSettings();
-
         this.addSettingTab(new RandomWalkNoteSettingTab(this));
 
         this.addCommand({
@@ -45,16 +49,26 @@ export default class RandomWalkNotePlugin extends Plugin {
         let fileToOpen = randomElement(filteredTags);
 
         if (alreadyOpen.includes(fileToOpen.name)) {
-          new Notice('Already: ' + fileToOpen.name);
           fileToOpen = randomElement(filteredTags);
+        }
+        // 如果文件还有重复，再查找两边
+        if (alreadyOpen.includes(fileToOpen.name)) {
+          fileToOpen = randomElement(filteredTags);
+        }
+        if (alreadyOpen.includes(fileToOpen.name)) {
+          fileToOpen = randomElement(filteredTags);
+        }
+
+        if (alreadyOpen.includes(fileToOpen.name)) {
+          // 查重三遍后忽略重复
         } else {
           alreadyOpen.push(fileToOpen.name);
         }
 
         if (alreadyOpen.length == filteredTags.length) {
           alreadyOpen.length = 0;
+          new Notice('Already completed all the note reviews');
         }
-        new Notice('Open: ' + fileToOpen.name + '; Count: ' + alreadyOpen.length);
         await this.app.workspace.openLinkText(fileToOpen.basename, '', this.settings.openInNewLeaf, {
             active: true,
         });
@@ -109,7 +123,7 @@ export default class RandomWalkNotePlugin extends Plugin {
         this.ribbonIconEl?.remove();
         if (this.settings.enableRibbonIcon) {
             this.ribbonIconEl = this.addRibbonIcon(
-                'footprints',
+                'random-walk-icon',
                 'Random walk note',
                 this.handleOpenRandomNote,
             );
